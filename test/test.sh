7992e9532e150212630badd7f0e865178cc7db30
Chris Malek <Christophe.Malek@canada.ca>
2016-09-07 14:36:06 +0000

    add setting of IDL_PATH so we can test the source that was just compiled

--- test/test.sh
+++ test/test.sh
@@ -3,11 +3,15 @@
 SCRIPT=$(readlink -f ${BASH_SOURCE-${.sh.file}})
 SCRIPT_PATH=$(dirname ${SCRIPT})
 
-# a little bit of help to let old ordenv find librmnshared
+# load environment
 . d.compile
+export IDL_PATH="../share/scripts:${IDL_PATH}"
+# a little bit of help to let old ordenv find librmnshared
 export LD_LIBRARY_PATH=/ssm/net/rpn/libs/15.2/${ORDENV_PLAT}/lib/Linux_x86-64/${COMP_ARCH}/:${SCRIPT_PATH}/../src/:${LD_LIBRARY_PATH};
 #echo ${LD_LIBRARY_PATH};
+
 output=$(r.idl -e test | grep BWTN6)
+
 if [[ "${output}" != "BWTN6    " ]]; then
     echo "ERROR: Unexpected output from test program"
     exit 1
