02c545bc9692f4e8ab7dbc093479b12fb026bcd2
Jose Garcia <jose.garcia@canada.ca>
2017-04-07 21:56:26 +0000

    bugfix?

--- test/test.sh
+++ test/test.sh
@@ -4,8 +4,9 @@ SCRIPT=$(readlink -f ${BASH_SOURCE-${.sh.file}})
 SCRIPT_PATH=$(dirname ${SCRIPT})
 
 # load environment
-. d.compile.ordenv.cfg
-. d.compile.intel.cfg
+#. d.compile.ordenv.cfg
+#. d.compile.intel.cfg
+. d.compile
 export IDL_PATH="../share/scripts:${IDL_PATH}"
 # a little bit of help to let old ordenv find librmnshared
 export LD_LIBRARY_PATH=/ssm/net/rpn/libs/15.2/${ORDENV_PLAT}/lib/Linux_x86-64/${COMP_ARCH}/:${SCRIPT_PATH}/../src/:${LD_LIBRARY_PATH};
