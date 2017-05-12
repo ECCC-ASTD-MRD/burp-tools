f3d1241de5f7017a8ba40f03f1c3135f59a8e1f0
Anthony Chartier <anthony.chartier@canada.ca>
2017-05-12 15:10:12 +0000

    Update
    Enlever un LD_LIBRARY_PATH rendu inutile (?).

--- test/test.sh
+++ test/test.sh
@@ -4,12 +4,12 @@ SCRIPT=$(readlink -f ${BASH_SOURCE-${.sh.file}})
 SCRIPT_PATH=$(dirname ${SCRIPT})
 
 # load environment
-#. d.compile.ordenv.cfg
-#. d.compile.intel.cfg
-. d.compile
+. d.compile.ordenv.cfg
+. d.compile.intel.cfg
+#. d.compile
 export IDL_PATH="../share/scripts:${IDL_PATH}"
 # a little bit of help to let old ordenv find librmnshared
-export LD_LIBRARY_PATH=/ssm/net/rpn/libs/${RMNLIB_DOMAIN}/${ORDENV_PLAT}/lib/Linux_x86-64/${COMP_ARCH}/:${SCRIPT_PATH}/../src/:${LD_LIBRARY_PATH};
+#export LD_LIBRARY_PATH=/ssm/net/rpn/libs/${RMNLIB_DOMAIN}/${ORDENV_PLAT}/lib/Linux_x86-64/${COMP_ARCH}/:${SCRIPT_PATH}/../src/:${LD_LIBRARY_PATH};
 #echo ${LD_LIBRARY_PATH};
 
 output=$(r.idl -e test | grep BWTN6)
