50b8527558cebf8e5f405030a756ab502ba15ba7
Jose Garcia <jose.garcia@canada.ca>
2019-06-13 15:07:17 +0000

    test/test.sh

--- test/test.sh
+++ test/test.sh
@@ -10,6 +10,7 @@ SCRIPT_PATH=$(dirname ${SCRIPT})
 export IDL_PATH="../share/scripts:${IDL_PATH}"
 # a little bit of help to let old ordenv find librmnshared
 #export LD_LIBRARY_PATH=/ssm/net/rpn/libs/${RMNLIB_DOMAIN}/${ORDENV_PLAT}/lib/Linux_x86-64/${COMP_ARCH}/:${SCRIPT_PATH}/../src/:${LD_LIBRARY_PATH};
+export LD_LIBRARY_PATH=../lib/:${LD_LIBRARY_PATH};
 #echo ${LD_LIBRARY_PATH};
 
 output=$(r.idl -e test | grep BWTN6)
