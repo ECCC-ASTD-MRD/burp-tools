ac034c8aec655e3e522ca5855e64e69d7436a97a
Anthony Chartier <ant000@science.gc.ca>
2019-10-21 13:33:43 +0000

    Ajustement pour les tests.

--- test/test.sh
+++ test/test.sh
@@ -8,12 +8,10 @@ SCRIPT_PATH=$(dirname ${SCRIPT})
 . d.compile.intel.cfg
 #. d.compile
 export IDL_PATH="../share/scripts:${IDL_PATH}"
-# a little bit of help to let old ordenv find librmnshared
-#export LD_LIBRARY_PATH=/ssm/net/rpn/libs/${RMNLIB_DOMAIN}/${ORDENV_PLAT}/lib/Linux_x86-64/${COMP_ARCH}/:${SCRIPT_PATH}/../src/:${LD_LIBRARY_PATH};
 export LD_LIBRARY_PATH=../lib/:${LD_LIBRARY_PATH};
-#echo ${LD_LIBRARY_PATH};
+export MA_TABLEBURP_PERSONNELLE=./tableburp
 
-output=$(export AFSISIO=/home/jmg007/AFSISIO; r.idl -e test | grep BWTN6)
+output=$(r.idl -e test | grep BWTN6)
 
 if [[ "${output}" != "BWTN6    " ]]; then
     echo "ERROR: Unexpected output from test program"
