ed7ea65dd3287cb7b20522527348a2e68bfd28de
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@canada.ca>
2023-02-14 21:27:09 +0000

    Removed debug traces

--- src/brpvoir/rai2char.c
+++ src/brpvoir/rai2char.c
@@ -6,12 +6,12 @@ int nc=*f_nc;
 int entier=*f_entier;
 int i; 
  
-  printf("Debug nc=%d\n",nc);
-  printf("Debug entier=%d\n",entier);
-  printf("Debug *chaine avant ='%c'\n",*chaine);
+//  printf("Debug nc=%d\n",nc);
+//  printf("Debug entier=%d\n",entier);
+//  printf("Debug *chaine avant ='%c'\n",*chaine);
   for (i=0; i<nc; i++) {
     *chaine = (entier >> (i*8)) & 0xFF;
-    printf("Debug *chaine='%c'\n",*chaine);
+//    printf("Debug *chaine='%c'\n",*chaine);
     chaine++;
   }
 }
